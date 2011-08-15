4970364a2f550ad0f056927fc5e78442ce5a955d
Eric Kever <lolbummer@gmail.com>
2011-08-15 14:58:29 -0400

    Comments n stuff

--- pirate.js
+++ pirate.js
@@ -1,13 +1,24 @@
-/*Issues
-input text and password placeholders:
+/*
+Issues:
+-input text and password placeholders:
 	What if the element's value needs to be accessed externally?  The current method is going to cause issues in that case.  Traversing upwards for a form isn't a good idea either.  Remember that it's not only form submits that the value will be read.
+
+There is no intent to support IE6 with this, only IE7+.
+
+Optimizations:
+-Performance:
+	In .html5.loader, the regex in the replace method shouldn't be instantiated OTF (on the fly), it should be compiled before it is called.  I just need to figure out where I'm going to put it.
+
+-Ease of use:
+	Any instance of the classes "pir-placeholder", "pir-placeholder-hidden", and "pir-placeholder-shown" should be put in the config (wherever I decide to put that) so that they can be configured by the developer.
+
 */
 
 (function(){
 	var Pirate = {  //Arrrrrrrrr
 		functions : {  //Alright, so I think it's the best idea here to NOT have to rely on a framework, so let's add in some custom cross-browser solutions here.
 					   //These should look familiar, I built them for Skeleton originally.
-			getElementsByClassName : function(context, cls){
+			getElementsByClassName : function(context, cls){  //This will mimic element.getElementsByClassName functionality for older browsers that do not support it, such as IE7.
 				if(context.getElementsByClassName){
 					return context.getElementsByClassName(cls);
 				}
@@ -31,7 +42,7 @@ input text and password placeholders:
 				return ret;
 			},
 			
-			filterTag : function(list, tag){
+			filterTag : function(list, tag){  //This will filter elements in a given NodeList based on either a tag string or an array of tags.
 				if(tag.push){
 					var exists = false;
 					for(var i = 0, j = tag.length; i < j; i++){
@@ -57,7 +68,7 @@ input text and password placeholders:
 				return list;
 			},
 			
-			addClass : function(element, name){
+			addClass : function(element, name){  //This will add the class(es) given in name to element, if they are not already present
 				var classes = (element.getAttribute("className") || element.className).split(" "),
 					exists = false;
 				
@@ -88,7 +99,7 @@ input text and password placeholders:
 				}
 			},
 			
-			removeClass : function(element, name){
+			removeClass : function(element, name){  //This will remove the class(es) given in name from the current element, if present
 				var classes = (element.getAttribute("className") || element.className).split(" "),
 					exists = false;
 				if(name.push){
@@ -118,12 +129,12 @@ input text and password placeholders:
 				}
 			},
 			
-			swapClass : function(element, from, to){
+			swapClass : function(element, from, to){  //Quick alias for element.removeClass(), element.addClass();
 				this.removeClass(element, from);
 				this.addClass(element, to);
 			},
 			
-			addListener : function(element, on, fn, last){
+			addListener : function(element, on, fn, last){  //Attaches an event listener to an element, compatible with IE7+8's faulty event handler system (when an event is attached with attachEvent, it is not called in the proper context, thus 'this' is actually 'window')
 				last = (last || false);
 				var BH;
 				
@@ -155,7 +166,7 @@ input text and password placeholders:
 				support : {
 					placeholder : 'placeholder' in document.createElement('input')
 				},
-				managedElements : {
+				managedElements : {  //Necessary so that redundant processing doesn't happen.
 					color : [],
 					email : [],
 					url : [],
@@ -235,7 +246,7 @@ input text and password placeholders:
 					}
 				},
 				
-				textPlaceholder : function(element){
+				textPlaceholder : function(element){  //Sets up placeholders in text inputs, attaches the proper event handlers.
 					if(element.value === ""){
 						element.value = element.getAttribute("placeholder");
 						Pirate.functions.addClass(element, "pir-placeholder");
@@ -264,7 +275,7 @@ input text and password placeholders:
 					Pirate.html5.form.managedElements.text.push(this);
 				},
 				
-				passPlaceholder : function(element){
+				passPlaceholder : function(element){  //Sets up placeholders in password inputs, attaches the proper event handlers.
 					var tempElement = document.createElement("input"),
 						passElement = element;
 					
@@ -290,7 +301,7 @@ input text and password placeholders:
 					
 					Pirate.addListener(passElement, "blur", function(){
 						if(this.value === ""){
-							tempElement.value = passElement.getAttribute("placeholder");  //Why this is here:  What if they changed the placeholder via JavaScript between the previous time and the current time this function was called?
+							tempElement.value = passElement.getAttribute("placeholder");  //Why this is here:  What if they changed the placeholder via JavaScript between the previous time and the current time this handler was called?  We need to ensure that the placeholder value is refreshed every time it is displayed.
 							Pirate.swapClass(tempElement, "pir-placeholder-hidden", "pir-placeholder-shown");
 							Pirate.swapClass(this, "pir-placeholder-shown", "pir-placeholder-hidden");
 							this._pirate.hasChanged = false;
@@ -305,7 +316,7 @@ input text and password placeholders:
 				}
 			},
 			
-			loader : function(){
+			loader : function(){  //Attaches HTML5 handlers to specific elements.
 				var type,
 					inputs = Pirate.html5.form.inputElements = document.getElementsByTagName("input");
 				
